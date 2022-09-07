ba9887c63c44ef93cff6e0430595fb23eba74828
Okikiola Omotosho <omotoshookiki@gmail.com>
2022-09-07 10:28:55 +0100

    Fixed the onboardig

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -523,14 +523,13 @@ const RootNavigator = ({ initialBoarded }) => {
     <AppStack.Navigator
       screenOptions={{ headerShown: false }}
       // initialRouteName="DepositSummary"
-      initialRouteName={!initialBoarded ? "Getstarted" : "Onboarding"}
+      initialRouteName={initialBoarded ? "Getstarted" : "Onboarding"}
     >
       {/* <AppStack.Screen name="map" component={Map} /> */}
       {/* SCREEN FOR AUTH */}
       {!token ? (
         <AppStack.Group screenOptions={verticalAnimation}>
           <AppStack.Screen name="Onboarding" component={Onboarding} />
-
           <AppStack.Screen name="Getstarted" component={Getstarted} />
 
           {/* <AppStack.Screen name="Onboarding" component={Onboarding} /> */}
