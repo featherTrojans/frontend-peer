0c2865bf2c466801304798141991871f0f87f877
Okikiola Omotosho <omotoshookiki@gmail.com>
2023-08-21 13:32:02 +0100

    BVN SCreen changes

--- screens/BVNScreen.tsx
+++ screens/BVNScreen.tsx
@@ -8,14 +8,26 @@ import {
 } from "../components";
 import { VALIDATION, navigation } from "../utils";
 import { useForm } from "react-hook-form";
-import { Text, View, TouchableOpacity, Pressable, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Pressable,
+  StyleSheet,
+} from "react-native";
 import { BVNScreenStyles } from "../assets/styles/screens";
 import { useAlert } from "../hooks";
 import axiosCustom from "../httpRequests/axiosCustom";
 import { AuthContext } from "../context/AuthContext";
 import { COLORS, FONTS, fontsize, icons } from "../constants";
-import Animated, { FadeIn, FadeOut, SlideInDown, SlideInUp, SlideOutDown, SlideOutUp } from "react-native-reanimated";
-
+import Animated, {
+  FadeIn,
+  FadeOut,
+  SlideInDown,
+  SlideInUp,
+  SlideOutDown,
+  SlideOutUp,
+} from "react-native-reanimated";
 
 const {
   headerText,
@@ -28,6 +40,9 @@ const {
   link,
   skipLaterText,
   bvnIconTextWrap,
+  modalBg,
+  modalBvnInfo,
+  modalBvnInfoText,
 } = BVNScreenStyles;
 
 const { Skiplatericon, Bvncommenticon, Bvnverifyicon } = icons;
@@ -38,8 +53,8 @@ function BVNScreen() {
   const [loading, setLoading] = useState(false);
   const { control, handleSubmit } = useForm({ mode: "all" });
   const { errorAlert } = useAlert();
-  const [showModal, setShowModal] = useState(false)
-  const [height, setHeight] = useState(56)
+  const [showModal, setShowModal] = useState(false);
+  const [height, setHeight] = useState(56);
 
   const onsubmit = async (data) => {
     try {
@@ -53,9 +68,9 @@ function BVNScreen() {
     }
   };
   const closeModal = () => {
-    setShowModal(s => !s)
-    setHeight(240)
-  }
+    setShowModal((s) => !s);
+    setHeight(240);
+  };
 
   const skipToWelcome = () => {
     navigation.navigate("welcome_screen", {
@@ -63,99 +78,97 @@ function BVNScreen() {
     });
   };
 
-
-
   const BVNReasonModal = () => {
     return (
-      <AnimatedPressable 
-      entering={FadeIn}
-      exiting={FadeOut}
-      onPress={closeModal}
-      style={{...StyleSheet.absoluteFillObject, backgroundColor: COLORS.halfBlack, zIndex: 1, paddingHorizontal: 16}}
+      <AnimatedPressable
+        entering={FadeIn}
+        exiting={FadeOut}
+        onPress={closeModal}
+        style={modalBg}
       >
-        <AnimatedPressable 
-        entering={SlideInDown}
-        exiting={SlideOutDown}
-        style={{position: "absolute", bottom: 80, left: 16, right: 16, height: height, backgroundColor: "#fff", borderRadius: 10}}
+        <AnimatedPressable
+          entering={SlideInDown}
+          exiting={SlideOutDown}
+          style={[modalBvnInfo, { height: height }]}
         >
-          <TouchableOpacity activeOpacity={0.8} onPress={closeModal} style={[bvnreason]}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={closeModal}
+            style={[bvnreason]}
+          >
             <View style={bvnIconTextWrap}>
               <Bvncommenticon />
               <Text style={bvntext}>Why Should I verify my BVN?</Text>
             </View>
-            <View style={{transform: [{rotate: "180deg"}]}}>
+            <View style={{ transform: [{ rotate: "180deg" }] }}>
               <Bvnverifyicon />
             </View>
           </TouchableOpacity>
 
-          <View style={{paddingHorizontal: 16}}>
-            
-          <Text style={{...fontsize.smallest, ...FONTS.medium, marginBottom: 25}}>1. Increased wallet limits </Text>
-          <Text style={{...fontsize.smallest, ...FONTS.medium, marginBottom: 25}}>2. Free bank account number for receiving money </Text>
-          <Text style={{...fontsize.smallest, ...FONTS.medium, marginBottom: 25}}>3. Access to discounted offers</Text>
+          <View style={{ paddingHorizontal: 16 }}>
+            <Text style={modalBvnInfoText}>1. Increased wallet limits </Text>
+            <Text style={modalBvnInfoText}>
+              2. Free bank account number for receiving money{" "}
+            </Text>
+            <Text style={modalBvnInfoText}>3. Access to discounted offers</Text>
           </View>
-
-          
-
-
         </AnimatedPressable>
-
       </AnimatedPressable>
-    )
-  }
+    );
+  };
   return (
     <>
-    <FTTitlepagewrapper title="Verify BVN">
-
-      <FTLoader loading={loading} />
-      <View style={flex}>
-        <Text style={headerText}>Hi Doyin</Text>
-        <Text style={headerText}>Enter your BVN</Text>
-        <FTInput
-          placeholderText="Enter BVN"
-          name="bvn"
-          label="Your 12 Digit BVN"
-          textInputProps={{
-            maxLength: 11,
-            keyboardType: "number-pad",
-            returnKeyType: "done",
-          }}
-          control={control}
-          rules={VALIDATION.BVN_NUMBER_INPUT_VALIDATION}
-          mB={20}
-          mT={50}
-        />
-        <FTCustombutton btntext="Continue" onpress={handleSubmit(onsubmit)} />
-        {!token && (
+      <FTTitlepagewrapper title="Verify BVN">
+        <FTLoader loading={loading} />
+        <View style={flex}>
+          <Text style={headerText}>Hi Doyin</Text>
+          <Text style={headerText}>Enter your BVN</Text>
+          <FTInput
+            placeholderText="Enter BVN"
+            name="bvn"
+            label="Your 12 Digit BVN"
+            textInputProps={{
+              maxLength: 11,
+              keyboardType: "number-pad",
+              returnKeyType: "done",
+            }}
+            control={control}
+            rules={VALIDATION.BVN_NUMBER_INPUT_VALIDATION}
+            mB={20}
+            mT={50}
+          />
+          <FTCustombutton btntext="Continue" onpress={handleSubmit(onsubmit)} />
+          {!token && (
+            <TouchableOpacity
+              activeOpacity={0.4}
+              style={skip}
+              onPress={skipToWelcome}
+            >
+              <Skiplatericon />
+              <Text style={skipLaterText}>Skip for later</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+        <View style={flexdown}>
           <TouchableOpacity
-            activeOpacity={0.4}
-            style={skip}
-            onPress={skipToWelcome}
+            activeOpacity={0.8}
+            onPress={closeModal}
+            style={bvnreason}
           >
-            <Skiplatericon />
-            <Text style={skipLaterText}>Skip for later</Text>
+            <View style={bvnIconTextWrap}>
+              <Bvncommenticon />
+              <Text style={bvntext}>Why Should I verify my BVN?</Text>
+            </View>
+            <Bvnverifyicon />
           </TouchableOpacity>
-        )}
-      </View>
-      <View style={flexdown}>
 
-
-        <TouchableOpacity activeOpacity={0.8} onPress={closeModal} style={bvnreason}>
-          <View style={bvnIconTextWrap}>
-            <Bvncommenticon />
-            <Text style={bvntext}>Why Should I verify my BVN?</Text>
-          </View>
-          <Bvnverifyicon />
-        </TouchableOpacity>
-
-        <Text style={infotext}>
-          For more information visit,
-          <Text style={link}> www.getfeather.africa</Text>
-        </Text>
-      </View>
-    </FTTitlepagewrapper>
-    {showModal && <BVNReasonModal />}
-
+          <Text style={infotext}>
+            For more information visit,
+            <Text style={link}> www.getfeather.africa</Text>
+          </Text>
+        </View>
+      </FTTitlepagewrapper>
+      {showModal && <BVNReasonModal />}
     </>
   );
 }
