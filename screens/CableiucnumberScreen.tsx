0e5db131cb6e1379759768e45dfc8d7addb7cd07
Ayobami Lawal <bamiayo90@gmail.com>
2023-09-26 14:56:39 +0100

    bills

--- screens/CableiucnumberScreen.tsx
+++ screens/CableiucnumberScreen.tsx
@@ -1,34 +1,124 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { CableiucnumberScreenStyles } from '../assets/styles/screens'
-import { FTCustombutton, FTInput, FTTitlepagewrapper } from '../components'
-import { useForm } from 'react-hook-form'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { CableiucnumberScreenStyles } from "../assets/styles/screens";
+import {
+  FTCustombutton,
+  FTDetailsModal,
+  FTInput,
+  FTLoader,
+  FTTitlepagewrapper,
+} from "../components";
+import { useForm } from "react-hook-form";
+import axiosCustom from "../httpRequests/axiosCustom";
+import Bluecardicon from "../assets/icons/Cardactionicons/Bluecardicon";
+import { useAlert } from "../hooks";
 
+const {} = CableiucnumberScreenStyles;
 
-const {} = CableiucnumberScreenStyles
+const CableiucnumberScreen = ({ navigation, route }) => {
+  const [accountinfo, setaccouninfo] = useState();
+  const biller = route?.params?.biller;
+  const price = route?.params?.price;
+  const { control, handleSubmit } = useForm({ mode: "all" });
+  const [content, setContent] = useState<any>({ child: null });
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setloading] = useState(false);
+  const { errorAlert } = useAlert();
 
-const CableiucnumberScreen = () => {
+  const switchModals = (value, data) => {
+    const action = async (pin) => {
+      try {
+        await axiosCustom.post("bills/subscribe", {
+          service: biller,
+          smartcard_number: data.iucnumber,
+          productCode: biller,
+          userPin: pin,
+          amount: price,
+          customerName: `${data.lastName} ${data.firstName}`,
+        });
+        navigation.navigate("transactionsuccess_screen");
+      } catch (err) {
+        throw err;
+      }
+    };
+    const onpress = () => {
+      navigation.navigate("transactionpin_screen", { action });
+    };
+    switch (value) {
+      case 0:
+        setContent({
+          child: (
+            <FTDetailsModal
+              modalTitle="Bill Details"
+              title={`${data.lastName} ${data.firstName}`}
+              info={data.iucnumber}
+              onPress={onpress}
+              Icon={Bluecardicon}
+              extraComponent={
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginBottom: 20,
+                  }}
+                ></View>
+              }
+            />
+          ),
+        });
+        setShowModal((s) => !s);
+        break;
 
-  const { control, handleSubmit } = useForm({mode: 'all'});
+      default:
+        break;
+    }
+  };
 
+  const onsubmit = async (data) => {
+    setloading(true);
+    try {
+      const response = await axiosCustom.post("/bills/details", {
+        package: biller,
+        decoderNo: data.iucnumber,
+      });
+      console.log(response);
+      const acctinfo = response.data.data;
+      if (!acctinfo?.firstName) {
+        throw { response: { data: { message: "Account not available" } } };
+      }
+
+      setaccouninfo(acctinfo);
+      //  if not a an object, return error
+      switchModals(0, { ...response.data.data, iucnumber: data.iucnumber });
+    } catch (err) {
+      errorAlert(err);
+    } finally {
+      setloading(false);
+    }
+  };
   return (
-    <FTTitlepagewrapper title='IUC Number'>
-      <FTInput 
-      label='Enter IUC Number'
-      placeholderText='Enter Number'
-      name="iucnumber"
-      control={control}
-      mB={20}
-      mT={20}
+    <FTTitlepagewrapper
+      modalChildren={content.child}
+      showModal={showModal}
+      setShowModal={setShowModal}
+      modalHeight={276}
+      title="IUC Number"
+    >
+      <FTLoader loading={loading} />
+      <FTInput
+        label="Enter IUC Number"
+        placeholderText="Enter Number"
+        name="iucnumber"
+        control={control}
+        mB={20}
+        mT={20}
       />
-      <FTCustombutton 
-      btntext='Continue'
-      onpress={() => console.log("Lot ges")}
-      />
+      <FTCustombutton btntext="Continue" onpress={handleSubmit(onsubmit)} />
     </FTTitlepagewrapper>
-  )
-}
+  );
+};
 
-export default CableiucnumberScreen
+export default CableiucnumberScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- screens/ChoosecableamountScreen.tsx
+++ screens/ChoosecableamountScreen.tsx
@@ -1,31 +1,94 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ChoosecableamountScreenStyles } from "../assets/styles/screens";
 import {
   FTCustombutton,
   FTIconwithtitleandinfo,
+  FTLoader,
   FTSearchinput,
   FTTitlepagewrapper,
 } from "../components";
 import { redirectTo } from "../utils";
 import { icons } from "../constants";
 import { useNavigation } from "@react-navigation/native";
+import axiosCustom from "../httpRequests/axiosCustom";
+import { FlatList } from "react-native-gesture-handler";
+import { useAlert } from "../hooks";
+import amountFormatter from "../utils/formatMoney";
 
 const { Bluecardicon } = icons;
 const {} = ChoosecableamountScreenStyles;
 
-const ChoosecableamountScreen = () => {
+function isObject(value) {
+  return typeof value === "object" && value !== null && !Array.isArray(value);
+}
+
+const ChoosecableamountScreen = ({ route }) => {
+  const biller = route?.params?.biller;
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(false);
+  const [billeramount, setbilleramount] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredbilleramount, setfilteredbilleramount] = useState([]);
+  const { errorAlert } = useAlert();
+  useEffect(() => {
+    getBillerAmounts();
+  }, []);
+
+  const getBillerAmounts = async () => {
+    setLoading(true);
+    try {
+      const response = await axiosCustom.get(`/bills/prices/${biller}`);
+      console.log(response.data);
+      // if response.data.data is an object, make it an array
+      let responseamount = [];
+      if (isObject(response.data.data)) {
+        responseamount.push(response.data.data);
+      } else {
+        responseamount = response.data.data;
+      }
+      setbilleramount(responseamount);
+      setfilteredbilleramount(responseamount);
+    } catch (err) {
+      errorAlert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handlesearch = (val) => {
+    setfilteredbilleramount(
+      billeramount.filter((el) =>
+        el?.name?.toLowerCase()?.includes(val?.toLowerCase())
+      )
+    );
+    setSearch(val);
+  };
   return (
     <FTTitlepagewrapper title="Choose Cable Amount">
-      <FTSearchinput placeholder="Search Biller" />
-
-      <FTIconwithtitleandinfo
-        bG="red"
-        title="XTRA-BB2 50GB (1 Month)"
-        info="N24,500"
-        onPress={() => navigation.navigate("cableiuc_screen")}
-        Icon={Bluecardicon}
+      <FTSearchinput
+        value={search}
+        onChange={handlesearch}
+        placeholder="Search Biller"
+      />
+      <FTLoader loading={loading} />
+      <FlatList
+        data={filteredbilleramount}
+        ItemSeparatorComponent={() => <View style={{ height: 28 }} />}
+        renderItem={({ item }) => {
+          const { name, price } = item;
+          return (
+            <FTIconwithtitleandinfo
+              bG="red"
+              title={name}
+              info={amountFormatter(price)}
+              onPress={() =>
+                navigation.navigate("cableiuc_screen", { biller, price })
+              }
+              Icon={Bluecardicon}
+            />
+          );
+        }}
       />
     </FTTitlepagewrapper>
   );
