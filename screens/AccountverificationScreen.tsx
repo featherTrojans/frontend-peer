9e5bc97c384d0bc622b0df66f4cb241d2a1a5f0f
Okikiola Omotosho <omotoshookiki@gmail.com>
2023-09-26 02:48:13 +0100

    Android scroll issue fixed

--- screens/AccountverificationScreen.tsx
+++ screens/AccountverificationScreen.tsx
@@ -166,10 +166,10 @@ const AccountverificationScreen = ({ route }) => {
           <FTCustombutton
             btntext={`Updrage to ${levelTitle}`}
             onpress={() => {
-              // if (userlevel < level - 1) {
-              //   errorAlert(null, "Please upgrade to Odogwu");
-              //   return;
-              // }
+              if (userlevel < level - 1) {
+                errorAlert(null, "Kindly upgrade to Odogwu to proceed.");
+                return;
+              }
               navigation.navigate(upgradeLocation);
             }}
           />
